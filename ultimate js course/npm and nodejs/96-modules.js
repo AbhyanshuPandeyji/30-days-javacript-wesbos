7732c5bfc0cfbaca67cb9f25c23f849245098119
Abhyanshu Pandey <abhyanshupandeyji@gmail.com>
2023-09-03 09:27:53 +0530

    the npm and nodejs file

---
+++ ultimate js course/npm and nodejs/96-modules.js
@@ -0,0 +1,4 @@
+console.log("Hey i am good");
+
+// after initializing the npm init - now you can install the npm 3rd party packages 
+// thats why the init is used to install the different packages 
